976a6816759027ec96353488b6609d3e0983d544
Héctor López Hidalgo <data_analitics_hlh@protonmail.com>
2025-04-30 14:52:44 +0200

    tutorial completed

--- TutorialConsoleApp/Program.cs
+++ TutorialConsoleApp/Program.cs
@@ -1,7 +1,4 @@
 ﻿
-
-
-
 namespace TutorialConsoleApp
 {
     class TutorialConsoleApp
@@ -23,6 +20,11 @@ namespace TutorialConsoleApp
             ForeachLoopStatement();
             ArraysDataTypes();
             Collections();
+            Methods();
+            Classes();
+            ExceptionHandling();
+            Console.WriteLine("\nPress any key to exit..."); 
+            Console.ReadKey(); 
         }
 
         static void PrintToConsole()
@@ -237,7 +239,7 @@ namespace TutorialConsoleApp
         static void ForeachLoopStatement()
         {
             Console.WriteLine("This is a foreach statements in C#");
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 10 };
+            int[] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 10 ];
 
             foreach (int number in numbers)
             {
@@ -259,7 +261,7 @@ namespace TutorialConsoleApp
         static void Collections()
         {
             Console.WriteLine("Collections: Complex Data Types in C#");
-            List<string> names = new List<string>();
+            List<string> names = [];
             names.Add("John");
             names.Add("Doe");
             names.Add("Jane");
@@ -275,7 +277,7 @@ namespace TutorialConsoleApp
                 Console.WriteLine(name);
             }
             Console.WriteLine("Now letś go with the hashsets.");
-            HashSet<float> hashset = new HashSet<float> {1,1,1,1,};
+            HashSet<float> hashset = [1,1,1,1,];
             hashset.Add(1.2f);
             hashset.Add(1.3f);
             hashset.Add(1.4f);
@@ -297,8 +299,157 @@ namespace TutorialConsoleApp
                 
             }
 
-        }
+            if (ages.ContainsKey("Doe"))
+            {
+                Console.WriteLine($" The key  is present.");
+            }
+            else
+            {
+                Console.WriteLine($"The key doesn't exist. This are the keys:");
+                foreach (string k in ages.Keys)
+                {
+                    Console.WriteLine(k);
+                }
+            }
+            Console.WriteLine("Now let's go with the queues.");
+            Console.WriteLine("Queues are first in  and first out structures");
+            Console.WriteLine("If I add anything or remove it from the queue, it will be always the first element.");
+            Queue<string> myQueueu = new Queue<string>();
+            myQueueu.Enqueue("First");
+            myQueueu.Enqueue("Second");
+            myQueueu.Enqueue("Third");
+
+            while (myQueueu.Count > 0)
+            {
+                string item = myQueueu.Dequeue();
+                Console.WriteLine(item);
+            }
             
+            Console.WriteLine("Now let's go with the Stacks.");
+            Console.WriteLine("Stacks are last in and first out estructure.");
+            Stack<string> myStacku = new Stack<string>();
+            
+            myStacku.Push("First");
+            myStacku.Push("Second");
+            myStacku.Push("Third");
+
+            while (myStacku.Count > 0)
+            {
+                string item = myStacku.Pop();
+                Console.WriteLine(item);
+            }
+            
+
+        }
+        static void Methods()
+        {
+            Console.WriteLine("Methods from C#. The represent the behaviour of the aplication. Let's invoke a method!");
+            SayHello();
+            SayHelloPublic();
+            int a = 23;
+            int b = 22;
+            int z = Add(a, b);
+            Console.WriteLine($"This is the result of calling a method inside a method, the add method returns {z}");    
+        }
+
+        private static void SayHello()
+        {
+            Console.WriteLine("Hello World! THis is a private method.");
+        }
+        public static void SayHelloPublic()
+        {
+            Console.WriteLine("Hello World! THis is a public method.");
+        }
+
+        private static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static void Classes()
+        {
+            Console.WriteLine("Classes for C#");
+            Person Peter = new Person("John", "Doe", 13);
+            string firstName = Peter.FirstName;
+            string lastName = Peter.LastName;
+            Console.WriteLine($"FirstName: {firstName}");
+            Console.WriteLine($"LastName: {lastName}");
+            Console.WriteLine($"Age: {Peter.Age}");
+            Console.WriteLine($"{Peter.Greeting(name: "Héctor")}");
+        }
+
+        static void ExceptionHandling()
+        {
+            Console.WriteLine("ExceptionHandling in C#");
+            int a = 10;
+            int b = 0;
+            try
+            {
+                int z = a /  b;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            List<string> shapes =["square", "triangle", "circle"];
+            try
+            {
+                Console.WriteLine(shapes[90]);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message); // if the type of exception is not exact, it will crash the program. Usse exception
+
+            }
+            finally
+            {
+                Console.WriteLine("all code has been executed.");
+            }
+        }
     }
+
+    public class Person
+    {
+        //Fields
+        private string _firstName;
+        private string _lastName;
+        private int _age;
+        //Constractors
+        public Person(string firstName, string lastName, int age)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _age = age;
+        }
+        
+        // Definir comportmiento, como set y get
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+            
+        }
+        public string LastName
+        
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        //aalterrnative way to set propertiess
+        
+        public string Name {get; set;}// IN this case I do not need the constractor
+
+        public string Greeting(string name)
+        {
+            return $"Hello, {name}!";
+        }
+    }
+    
 }
 
